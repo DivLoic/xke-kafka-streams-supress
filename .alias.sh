5013c353b06bc0e0d98cd588bcdc436abc382dcc
DivLoic <ldivad@isep.fr>
2020-01-04 15:58:44 +0100

    Move from python scripts to bash scripts

--- .alias.sh
+++ .alias.sh
@@ -1,6 +1,6 @@
 
-alias wfr1='doitlive play -q ./.app.py'
-alias wfr2='doitlive play -q ./.clock.py'
-alias wfr3='doitlive play -q ./.input.py'
-alias wfr4='doitlive play -q ./.output.py'
+alias wfr1='doitlive play -q ./.app.sh'
+alias wfr2='doitlive play -q ./.clock.sh'
+alias wfr3='doitlive play -q ./.input.sh'
+alias wfr4='doitlive play -q ./.output.sh'
 alias wfr-clean='./.clean-up.sh'
